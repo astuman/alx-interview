81b3e8dfc61f180a719179faf743a5bc9153349b
Astewul <loloalex1992@gmail.com>
2023-01-26 13:04:02 +0300

    update pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,7 +2,7 @@
 # pascal_triangle.py
 def pascal_triangle(n):
     if n <= 0:
-        return []
+        return [""]
     for i in range(n, n+1):
         for j in range(0, n-i+1):
             print('', end='')
