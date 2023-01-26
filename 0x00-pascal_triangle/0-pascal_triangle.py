72df73ee34362286c58923c113caf9580bbe8f06
Astewul <loloalex1992@gmail.com>
2023-01-26 12:40:52 +0300

    add pascal tringle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,12 @@
 def pascal_triangle(n):
     if n <= 0:
-        return [];
+        return []
     else:
         for i in range(n, n+1):
             for j in range(0, n-i+1):
                 print('', end='')
             c = 1
             for j in range(1, i+1):
-                print('', c, sep='',end='')
-                cc=c*(i-j)//j
+                print('', c, sep='', end='')
+                cc = c * (i-j) // j
             print()
